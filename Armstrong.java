36ea1080c81dbd9f7a21d6ed98a3f319338d9404
AnsDark <73783593+AnsDark@users.noreply.github.com>
2021-06-14 14:03:51 +0530

    Programs

---
+++ Armstrong.java
@@ -0,0 +1,20 @@
+package ansari2;
+
+public class Armstrong {
+	public static void main(String[] args)
+	{
+		int number=147,originalNumber,remainder,result=0;
+		originalNumber=number;
+		while(originalNumber !=0)
+		{
+			remainder=originalNumber %10;
+			result +=Math.pow(remainder,3);
+			originalNumber /=10;
+		}
+		if(result==number)
+			System.out.println(number+"is an armstrong.");
+		else
+			System.out.println(number+"is not an armstrong.");
+	}
+
+}
